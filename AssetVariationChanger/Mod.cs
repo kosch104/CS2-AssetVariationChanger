ec58775b3aaca0520de7c9fbfff47e1cbbb696d8
kosch104 <konstantin-schwarze@outlook.de>
2024-04-14 17:52:02 +0200

    Initial commit

---
+++ AssetVariationChanger/Mod.cs
@@ -0,0 +1,43 @@
+﻿using AssetVariationChanger.Systems;
+using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using Colossal.IO.AssetDatabase;
+
+namespace AssetVariationChanger
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(AssetVariationChanger)}.{nameof(Mod)}")
+            .SetShowsErrorsInUI(false);
+
+        private Setting m_Setting;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+            AssetDatabase.global.LoadSettings(nameof(AssetVariationChanger), m_Setting, new Setting(this));
+
+            updateSystem.UpdateBefore<RandomSeedSystem>(SystemUpdatePhase.Modification1);
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null)
+            {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+    }
+}
---
+++ AssetVariationChanger/Systems/RandomSeedSystem.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="TreeObjectDefinitionSystem.cs" company="Yenyangs Mods. MIT License">
+// Copyright (c) Yenyangs Mods. MIT License. All rights reserved.
+// </copyright>
+
+using Colossal.Entities;
+using Colossal.Logging;
+using Game;
+using Game.Common;
+using Game.Tools;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine.InputSystem;
+
+namespace AssetVariationChanger.Systems
+{
+
+
+    /// <summary>
+    /// Overrides tree state on placement with object tool based on setting.
+    /// </summary>
+    public partial class RandomSeedSystem : GameSystemBase
+    {
+        private EntityQuery m_ObjectDefinitionQuery;
+        private ILog m_Log;
+        private Unity.Mathematics.Random m_Random;
+        private int m_RandomSeed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomSeedSystem"/> class.
+        /// </summary>
+        public RandomSeedSystem()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_Log = Mod.log;
+            m_Log.Info($"[{nameof(RandomSeedSystem)}] {nameof(OnCreate)}");
+            m_Random = new Unity.Mathematics.Random(1);
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnUpdate()
+        {
+            m_ObjectDefinitionQuery = SystemAPI.QueryBuilder()
+                .WithAllRW<CreationDefinition>()
+                .WithAll<Updated>()
+                .WithNone<Deleted, Overridden>()
+                .Build();
+
+            RequireForUpdate(m_ObjectDefinitionQuery);
+            NativeArray<Entity> entities = m_ObjectDefinitionQuery.ToEntityArray(Allocator.Temp);
+
+            foreach (Entity entity in entities)
+            {
+                if (!EntityManager.TryGetComponent(entity, out CreationDefinition currentCreationDefinition))
+                {
+                    entities.Dispose();
+                    return;
+                }
+
+                if (Keyboard.current.insertKey.isPressed) 
+                {
+                    m_RandomSeed = m_Random.NextInt();
+                    m_Log.Info($"InsertKeyPressed {m_RandomSeed}");
+                }
+                currentCreationDefinition.m_RandomSeed = m_RandomSeed;
+                EntityManager.SetComponentData(entity, currentCreationDefinition);
+            }
+
+            entities.Dispose();
+        }
+
+    }
+}
